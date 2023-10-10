389b31d17e81bd7171344da5a4389f89239cad6d
davidlaekur <davidlaekur@me.com>
2023-10-11 01:34:17 +0200

    Add files via upload

---
+++ Ej10.php
@@ -0,0 +1,14 @@
+<?php
+    
+
+  /*   10. EJERCICIO 10
+    Crear un array multidimensional con los siguientes datos de hoteles:
+          
+     Y realiza las siguientes acciones (cada una de las acciones deberán implementarse en funciones):
+    • Mostrar el listado de hoteles
+    • Mostrar el listado de hoteles de más de 100 habitaciones
+    • Mostrar el listado de hoteles de menos de 100 habitaciones y 3 estrellas
+    • Borrar el hotel Acta del Carmen y mostrar el listado de hoteles
+    • Borrar todos los hoteles y mostrar un mensaje “No hay hoteles en la base de datos”,
+    al intentar mostrar el listado de hoteles
+    • Añadir los siguientes hoteles al array y mostrar el listado */
